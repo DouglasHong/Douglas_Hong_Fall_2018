60caf9ce91a3f2e8b13c2331d299368c4877ec8e
APCS2nd <APCS2nd@MHS-Dreyer-08>
2018-08-24 09:01:02 -0700

    Version2.0 Hello World!

--- Hello/src/Hello_World.java
+++ Hello/src/Hello_World.java
@@ -1,12 +1,10 @@
-
-public class Hello_World {
-	/**
-	 * Print out a message saying Hello World! From Douglas
-	 * 
+/**
+	 * Prints out a message to the console window saying "Hello World! From Douglas" 
+	 *
 	 * @author Douglas Hong
 	 * @version 8/17/2018
 	 */
-
+public class Hello_World {
 	public static void main(String[] args) {
 		System.out.println("Hello World! From Douglas");
 	}
