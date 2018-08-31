953eb55a6a9735885da8be329355a0e0fc1d6cc6
APCS2nd <APCS2nd@MHS-Dreyer-08>
2018-08-31 09:51:17 -0700

    Start of Pikachu program

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,19 @@
+/**
+	 * Prints out a message to the console window saying  
+	 * Pikachu welcomes you to the world of Pokemon!
+	 * 					(\__/)
+	 * 					(o^.^)
+	 * 			  	   z(_(")(")
+	 *
+	 * @author Douglas Hong
+	 * @version 8/31/2018
+	 */
+public class Pikachu {
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t\t\t(\\__/)");
+		System.out.println("\t\t\t\t(o^.^)");
+	
+	}
+
+}
